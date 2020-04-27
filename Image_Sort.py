4a7cae06052f7cfa7aafb0cab7d7d0cb4dcbd5d3
Daniel Dreise <ddreise@gmail.com>
2020-04-26 22:26:55 -0400

    Creates new directory

--- Image_Sort.py
+++ Image_Sort.py
@@ -6,21 +6,59 @@
 import sys
 import os
 import datetime
+import pathlib
+import string
+import shutil
+
+from stat import *
 
 # Open folder
-print ("File to be sorted:", sys.argv[1])
 
-imageDirectory = sys.argv[1]
+imageDirectory = pathlib.Path(sys.argv[1])
+print ("File to be sorted:", imageDirectory)
 
 # Ensure validity of user input (is it a folder?)
+if os.path.exists(imageDirectory):
+    print("Path exists")
+    if os.path.isdir(imageDirectory):
+        print("It is a directory")
+    else:
+        print("It is not a directory")
+        exit()
+else:
+    print("The path does not exist")
+    exit()
 # Ask user if folder is correct, if "y" continue, if "n" cancel
 
 # Loop through images
 for image in os.scandir(imageDirectory):
     info = image.stat()
-    mtime = info.st_mtime
-    timestamp_str = datetime.datetime.fromtimestamp(mtime).strftime('%Y-%m-%d-%H:%M')
-    print('Timestamp: ', str(timestamp_str))
+    mode = image.stat().st_mode
+
+    # Do if regular file
+    if S_ISREG(mode):
+        mtime = info.st_mtime
+
+        # Parse information to get month and year
+        timestamp = datetime.datetime.fromtimestamp(mtime).strftime('%Y_%B')
+
+        # If directory doesn't exists, create directory with corresponding month and year
+        print(imageDirectory / timestamp)
+        if not os.path.exists(imageDirectory / timestamp):
+            os.mkdir(imageDirectory / timestamp)
+            print("Folder created ", imageDirectory / timestamp)
+
+        # Move image into new directory
+        print ("Image to be moved: ", / imageDirectory / image)
+        print ("Directory to move to: ", / imageDirectory / timestamp)
+        shutil.move("C:" / imageDirectory / image, "C:" / imageDirectory / timestamp)
+
+
+    # Ignore if hidden or unique file
+    else:
+        print('Skipped')
+
+
 #   Check image properties (specifically month and year)
 #   If no month/year, put into "No Date" folder
 #   Put into year/month folder
